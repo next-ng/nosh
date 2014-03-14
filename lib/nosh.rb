d1574c12442f9d83af46e6a291bddbe6bf1fcc62
Abhijit Hiremagalur <abhi@pivotallabs.com>
2014-03-14 15:08:25 -0700

    Multiple components in a Releasefile
    Signed-off-by: Adam Stegman <astegman@pivotallabs.com>

--- lib/nosh.rb
+++ lib/nosh.rb
@@ -14,39 +14,33 @@ class Nosh
 
     system!("cp -r #{@source_release_path}/ #{@dest_release_path}")
 
-    processes = Dir.glob("#{component_path}/.nosh/*").map {|path| File.basename(path)}
-    processes.each do |process_name|
-      if Dir.exist?("#{component_path}/.nosh/#{process_name}/job")
-        # copy job
-        FileUtils.mkpath("#{@dest_release_path}/jobs/#{component_name}-#{process_name}")
-        system!("cp -r #{component_path}/.nosh/#{process_name}/job/ #{@dest_release_path}/jobs/#{component_name}-#{process_name}/")
+    components.each do |component_name, component_path|
+      processes = Dir.glob("#{component_path}/.nosh/*").map {|path| File.basename(path)}
+      processes.each do |process_name|
+        if Dir.exist?("#{component_path}/.nosh/#{process_name}/job")
+          # copy job
+          FileUtils.mkpath("#{@dest_release_path}/jobs/#{component_name}-#{process_name}")
+          system!("cp -r #{component_path}/.nosh/#{process_name}/job/ #{@dest_release_path}/jobs/#{component_name}-#{process_name}/")
+        end
+
+        if Dir.exist?("#{component_path}/.nosh/#{process_name}/package")
+          # copy package
+          FileUtils.mkpath("#{@dest_release_path}/packages/#{component_name}-#{process_name}")
+          system!("cp -r #{component_path}/.nosh/#{process_name}/package/ #{@dest_release_path}/packages/#{component_name}-#{process_name}/")
+        end
       end
 
-      if Dir.exist?("#{component_path}/.nosh/#{process_name}/package")
-        # copy package
-        FileUtils.mkpath("#{@dest_release_path}/packages/#{component_name}-#{process_name}")
-        system!("cp -r #{component_path}/.nosh/#{process_name}/package/ #{@dest_release_path}/packages/#{component_name}-#{process_name}/")
-      end
+      # copy source
+      FileUtils.mkpath("#{@dest_release_path}/src/#{component_name}")
+      system!("rsync -a --exclude=\".*\" #{component_path}/ #{@dest_release_path}/src/#{component_name}/")
     end
-
-    # copy source
-    FileUtils.mkpath("#{@dest_release_path}/src/#{component_name}")
-    system!("rsync -a --exclude=\".*\" #{component_path}/ #{@dest_release_path}/src/#{component_name}/")
   end
 
   def system!(cmd)
     system(cmd) or raise "Failed to run: `#{cmd}'"
   end
 
-  def component_name
-    @component_name ||= release_file.keys.first
-  end
-
-  def component_path
-    @component_path ||= release_file.values.first
-  end
-
-  def release_file
-    @release_file ||= YAML.load_file(File.join(@source_release_path, 'Releasefile'))
+  def components
+    @components ||= YAML.load_file(File.join(@source_release_path, 'Releasefile'))
   end
 end
